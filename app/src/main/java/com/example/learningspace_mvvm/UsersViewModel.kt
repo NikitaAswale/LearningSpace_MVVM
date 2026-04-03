9c6f524cf87703dac30996e503a9f2a942a1ca79
nikitaradhakrishnaaswale <aswalenikita26@gmail.com>
2026-04-03 20:05:38 +0530

    modified ui

--- app/src/main/java/com/example/learningspace_mvvm/UsersViewModel.kt
+++ app/src/main/java/com/example/learningspace_mvvm/UsersViewModel.kt
@@ -9,17 +9,27 @@ import kotlinx.coroutines.launch
 class UsersViewModel : ViewModel() {
 
     private val repository = Repository()
+    private val repository1 = Repository()
 
     private val _users = MutableStateFlow<List<Users>>(emptyList())
+    private val _users1 = MutableStateFlow<List<Users1>>(emptyList())
     val users: StateFlow<List<Users>> = _users
+    val users1: StateFlow<List<Users1>> = _users1
 
     init {
         fetchUsers()
+        fetchUsers1()
     }
 
     fun fetchUsers() {
         viewModelScope.launch {
             _users.value = repository.getUsers()
+        }
+    }
+
+    fun fetchUsers1() {
+        viewModelScope.launch {
+            _users1.value = repository1.getUsers1()
         }
     }
 
